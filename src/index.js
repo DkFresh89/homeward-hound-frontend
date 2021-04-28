d2f909d5ed7202617a77492f060350365d541031
Doug <doug.kerzner@gmail.com>
2021-04-28 11:41:07 -0400

    Chakra & Router Installed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react"
 
 ReactDOM.render(
+  <Router>
   <React.StrictMode>
+    <ChakraProvider>
     <App />
-  </React.StrictMode>,
+    </ChakraProvider>
+  </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
